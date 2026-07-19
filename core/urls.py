4605ec80ac01937dc8a467add8a1057b7100c8c8
Jiang Salim <jaingsalim@gmail.com>
2026-07-19 16:25:34 +0300

    Remove temporary test routes and simple_404

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
 from . import api_views
-from django.shortcuts import render
-
-def simple_404(request, exception=None):
-    return render(request, 'errors/404.html', status=404)
 
 urlpatterns = [
     path('import-students/', views.import_students, name='import_students'),
@@ -18,7 +14,5 @@ urlpatterns = [
     path('api/public/statement/', api_views.public_statement_pdf, name='public_statement'),
     path('api/public/balance-by-card/', api_views.public_balance_by_card, name='public_balance_by_card'),
     path('api/public/statement-by-card/', api_views.public_statement_by_card_pdf, name='public_statement_by_card'),
-    path('test-404/', views.error_404, name='test_404'),
-    path('test-500/', views.error_500, name='test_500'),
     path('backup/', views.backup_now, name='backup_now'),
 ]
