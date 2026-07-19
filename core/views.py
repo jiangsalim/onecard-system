12e344960e599f7d1f2918d1a9488a05a5246f0a
Jiang Salim <jaingsalim@gmail.com>
2026-07-19 15:09:57 +0300

    Fix settings import typo

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.conf import settingss
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Student
